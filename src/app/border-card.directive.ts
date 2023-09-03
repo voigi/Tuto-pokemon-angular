c891378aa52454be778d07f7b95bf01adf8dfcb2
voigi <supervoigi@gmail.com>
2023-09-03 22:34:15 +0200

    ajout d'un pipe personalisé

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,31 +1,35 @@
-import { Directive, ElementRef, HostListener, Input } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: "[pkmnBorderCard]",
+  selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
 
-   private inialColor :string  = '#f5f5f5';
-   private defaultColor :string  = '#009688';
-   private defaultHeight :number  = 180;
-
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 200;
 
   constructor(private el: ElementRef) {
     this.setHeight(this.defaultHeight);
-    this.setBorder(this.inialColor);
+    this.setBorder(this.initialColor);
   }
-  @Input("pkmnBorderCard") borderColor: string;
-  @HostListener("mouseenter") onMouseEnter() {
+
+  @Input('pkmnBorderCard') borderColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
     this.setBorder(this.borderColor || this.defaultColor);
   }
-  @HostListener("mouseleave") onMouseLeave() {
-    this.setBorder(this.inialColor);
+  
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder(this.initialColor);
   }
-  private setHeight(height: number) {
+
+  setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
 
-  private setBorder(color: string) {
-    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`; 
   }
+
 }
